482cd9eda92e75202e97367845d3164c5c5876de
chikako <sunchan415@gmail.com>
2019-11-03 16:42:07 +0900

    タイトル　　classのインスタンス化 　 詳細　＃インスタンス変数　属性（height,weight） 詳細 #インスタンスメソッド　メソッド（calculate_bmi）

---
+++ BMI.py
@@ -0,0 +1,32 @@
+'''
+class (成人の)BMI:
+    関連しそうな属性：
+    　　　ー身長
+         ー体重
+         ーBMIという値そのもの
+　　 ルール：
+         ー１０以上４０以下<--常識的な範囲
+         ー表示するときは、少数第2位まで
+            -ex: 23.6788 -> 23.67
+            -ex: 23.671 -> 23.67
+    できること：
+    　　　ー？？？
+'''
+
+
+# クラス名はUpperCamellCaseが普通
+class BMI:
+    def __init__(self, height, weight):
+        self.height = height
+        self.weight = weight
+
+    def calculate_bmi(self):
+        return self.weight / (self.height ** 2)
+
+
+# BMIクラスのインスタンス化
+hibiki_bmi = BMI(height=1.80, weight=67.0)
+noriya_bmi = BMI(height=1.78, weight=75.0)
+
+print(hibiki_bmi.height, hibiki_bmi.weight)
+print(hibiki_bmi.calculate_bmi())
